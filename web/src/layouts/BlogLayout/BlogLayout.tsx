eb359bd27bdc2b2893cdf4ebc8a89df947da1622
daniel-escoto <escotodanny62@gmail.com>
2022-10-28 13:13:57 -0700

    init

---
+++ web/src/layouts/BlogLayout/BlogLayout.tsx
@@ -0,0 +1,57 @@
+import { useAuth } from '@redwoodjs/auth'
+import { Link, routes } from '@redwoodjs/router'
+
+type BlogLayoutProps = {
+  children?: React.ReactNode
+}
+
+const BlogLayout = ({ children }: BlogLayoutProps) => {
+  const { logOut, isAuthenticated, currentUser } = useAuth()
+
+  return (
+    <>
+      <header>
+        <div className="flex-between">
+          <h1>
+            <Link to={routes.home()}>Redwood Blog</Link>
+          </h1>
+          {isAuthenticated ? (
+            <div>
+              <span>{currentUser.email}</span>
+              <button type="button" onClick={logOut}>
+                Log Out
+              </button>
+            </div>
+          ) : (
+            <nav>
+              <ul>
+                <li>
+                  <Link to={routes.login()}>Log In</Link>
+                </li>
+                <li>
+                  <Link to={routes.signup()}>Sign Up</Link>
+                </li>
+              </ul>
+            </nav>
+          )}
+        </div>
+        <nav>
+          <ul>
+            <li>
+              <Link to={routes.home()}>Home</Link>
+            </li>
+            <li>
+              <Link to={routes.about()}>About</Link>
+            </li>
+            <li>
+              <Link to={routes.contact()}>Contact</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>{children}</main>
+    </>
+  )
+}
+
+export default BlogLayout
